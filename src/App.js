52602f9f6c783bcba40c27c463cb0f02ee02f05a
doch13_ <dohui0922@dimigo.hs.kr>
2022-10-10 01:26:26 +0900

    feat: Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useEffect } from "react";
 import Survey from "./components/Survey";
 import Start from "./components/Start";
 import Result from "./components/Result";
 import { RecoilRoot } from "recoil";
+import ReactGA from "react-ga";
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("UA-237880535-1");
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <RecoilRoot>
       <BrowserRouter>
