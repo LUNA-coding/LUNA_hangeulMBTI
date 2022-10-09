f37208749cdd4188dcc70b97b9f66b425a52eb43
오명훈 <omh02033@gmail.com>
2022-10-09 20:14:44 +0900

    feat: result page input phonenumber component

--- src/components/Result.js
+++ src/components/Result.js
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { useLocation, useSearchParams } from "react-router-dom";
 import { Link, Route, BrowserRouter as Router } from 'react-router-dom';
 import styles from './result.css'
@@ -7,6 +8,7 @@ import { CopyToClipboard } from 'react-copy-to-clipboard';
 function Result() {
     const location = useLocation();
     const [searchMbti, setSearchMbti] = useSearchParams();
+    const [phone, setPhone] = useState('');
     const mbti = searchMbti.get('yourMBTIis');
     let index;
 
@@ -33,7 +35,25 @@ function Result() {
 
     console.log(data[index].img)
 
-    
+    const phoneNumSubmit = () => {
+        if (!phone) return alert('전화번호를 입력해주세요.');
+        fetch('https://api.mbti.hangeul.luna.codes/', {
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                phoneNum: phone
+            }),
+        }).then(res => res.json())
+        .then(data => {
+            if (data.phoneNum === phone) {
+                alert('전화번호가 저장되었습니다.');
+            } else {
+                alert('서버에 에러가 발생하였습니다.');
+            }
+        });
+    }
 
     return (
         <div className="result">
@@ -76,7 +96,15 @@ function Result() {
                         나와 닮은 한글 위인 찾기 테스트를 재밌게 진행하셨다면, <span>저희 프로젝트에 참여하여 함께 한글을 알려보아요!</span>
                     </div>
                     <div className="pro-sub">루나의 프로젝트에 관심이 있다면?</div>
-                    <a href="https://forms.gle/khj3oERSnrc98b399"><div className="btn pro">1분만 투자해서 응원해주기</div></a>
+                    <div className="funding">
+                        <div className="description">펀딩 시작시 메세지 받기</div>
+                        <div className="phoneForm">
+                            <input className="phoneNum" placeholder='전화번호를 입력해주세요(숫자만)' onChange={({ target: { value } }) => setPhone(value)} />
+                            <div className="phoneSub" style={{
+                                background: phone ? '#483278' : '#969696'
+                            }} onClick={phoneNumSubmit}>확인</div>
+                        </div>
+                    </div>
                 </div>
 
                 <hr />
